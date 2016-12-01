f0d376ed17ea91ba6b2dc4e3be0d0bc4d18255d3
Betty <betty@lytana.net>
2016-12-01 09:43:01 +0100

    Initial commit

---
+++ app/components/TodoApp.jsx
@@ -0,0 +1,13 @@
+var React = require('react');
+
+var TodoApp = React.createClass({
+	render: function () {
+		return (
+			<div>
+				<h1>TodoApp.jsx</h1>
+			</div>
+		);
+	}
+});
+
+module.exports = TodoApp;
