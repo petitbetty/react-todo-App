8fc522ec4a8d4995c37dec6f8614f2cc90700b08
Betty <betty@lytana.net>
2016-12-05 17:02:49 +0100

    Style todo application

--- app/tests/components/todoList.test.jsx
+++ app/tests/components/todoList.test.jsx
@@ -30,5 +30,14 @@ describe('TodoList', () => {
 
 		expect(todosComponents.length).toBe(todos.length);
 	});
+
+	it('should render emplty message if no todos', () => {
+
+		var todos = [];
+		var todoList = TestUtils.renderIntoDocument(<TodoList todos={todos} />);
+		var $el = $(ReactDOM.findDOMNode(todoList));
+
+		expect($el.find('.container__message').length).toBe(1);
+	});
 	
 });
